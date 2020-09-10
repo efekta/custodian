e8c92c4b3510acd3b977e69fc88741f89215dc58
git config --global user.name <ptashkina.l@investcustodian.ae>
2020-09-10 16:37:12 +0300

    build

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # custodian
+<p><a href="https://efekta.github.io/custodian/index.html">Главная</a></p>
+<p><a href="https://efekta.github.io/custodian/coworking-page.html">Коворкинг</a></p>
+<p><a href="https://efekta.github.io/custodian/coaching-page.html">Коучинг</a></p>
---
+++ js/common.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+  /*================================
+Плавный скролл до якоря jquery
+=================================*/
+    $("body").on('click', '[href*="#"]', function(e){
+      var fixed_offset = 100;
+      $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+      e.preventDefault();
+    });
+    /*========================
+     mob menu
+     =======================*/
+    $(".menu-toggle").on('click', function() {
+        $(this).toggleClass("on");
+        $('.nav-list').toggleClass("show");
+        // $('.nav').toggleClass("nav_hide");
+        $('body').toggleClass('overflow');
+    });
+    jQuery(window).width() <= 992 && $(".nav-list li").on('click', function() {
+        $('.menu-toggle').removeClass("on");
+        $('.nav-list').removeClass("show");
+        // $('.nav').removeClass("nav_hide");
+        $('body').removeClass('overflow');
+    });
+
+    /**/
+    $('#company-link_1').on('click', function() {
+        $('#company_2').removeClass('open');
+        $('#company_3').removeClass('open');
+        $('#company_1').toggleClass('open');
+        // $('.coaching-company-item').addClass('open');
+    });
+    /**/
+    $('#company-link_2').on('click', function() {
+        $('#company_1').removeClass('open');
+        $('#company_3').removeClass('open');
+        $('#company_2').toggleClass('open');
+        // $('.coaching-company-item').addClass('open');
+    });
+    /**/
+    $('#company-link_3').on('click', function() {
+        $('#company_1').removeClass('open');
+        $('#company_2').removeClass('open');
+        $('#company_3').toggleClass('open');
+        // $('.coaching-company-item').addClass('open');
+    });
+
+
+    /**/
+    $('#coaching-company-link_1').on('click', function() {
+        $('#coaching-company_2').removeClass('open');
+        $('#coaching-company_3').removeClass('open');
+        $('#coaching-company_1').toggleClass('open');
+        // $('#coaching-company-item_1').addClass('open');
+    });
+    /**/
+    $('#coaching-company-link_2').on('click', function() {
+        $('#coaching-company_1').removeClass('open');
+        $('#coaching-company_3').removeClass('open');
+        $('#coaching-company_2').toggleClass('open');
+        // $('#coaching-company-item_2').addClass('open');
+    });
+    /**/
+    $('#coaching-company-link_3').on('click', function() {
+        $('#coaching-company_1').removeClass('open');
+        $('#coaching-company_2').removeClass('open');
+        $('#coaching-company_3').toggleClass('open');
+        // $('#coaching-company-item_3').addClass('open');
+    });
+});
